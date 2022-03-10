761966303cf69e69bea27d871b1b562184b25ccf
cesarsiguencia <cesar.siguencia@gmail.com>
2022-03-10 18:20:51 -0500

    adding db.query statements to manipulate candidates table

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require("express");
 
 const PORT = process.env.PORT || 3010;
@@ -8,11 +10,59 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// this code will connect us to the database
+const db = mysql.createConnection({
+    host:  "localhost",
+    //your mysql username
+    user: "root",
+    //your mysql password
+    password: "holes360",
+    database: "election"
+},
+console.log("Connected to the election database")
+);
+
 app.get("/", (req, res) => {
     res.json({
         message: "Hello World"
     });
 })
+
+
+//THIS QUERY COMMAND SHOWS US ALL CANDIDATES
+// db.query(`SELECT * FROM candidates` , (err, rows) => {
+//     console.log(rows);
+// })
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row)
+// })
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//     VALUES (?,?,?,?)`;
+
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+
 
 
 // Default response for any other request (Not Found)
