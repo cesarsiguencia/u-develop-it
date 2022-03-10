7528947e3e356ceae37157bb884d27895b85b999
cesarsiguencia <cesar.siguencia@gmail.com>
2022-03-09 21:56:51 -0500

    added working server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3010;
+const app = express();
+
+//EXPRESS middleware
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello World"
+    });
+})
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+})
+
+
+
+//STARTING EXPRESS.js server through a port
+app.listen(PORT, () => { 
+    console.log(`Server running on port ${PORT}`);
+});
